b53f45359a5f91adcc841f235b67ad2b5b915cb3
harzeezco <wakilabdulazeez@gmail.com>
2024-04-12 17:08:10 +0100

    start development

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-[#292929] text-[#BABABA]`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
